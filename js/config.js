aa1f77310ae90b9baade129df8df425e1f929d02
Damian <themrdamus@gmail.com>
2018-07-31 13:32:09 +0100

    separated const and let variables

--- js/config.js
+++ js/config.js
@@ -1,17 +1,14 @@
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
     canvas.style.backgroundColor='lightblue';
-
-
-let width = 360,
+    width = 360,
     height = 400,
-    x = width/2,
-    y = height/2,
-    radius = 10,
-    velY = 0,
-    velX = 0,
     speed = 3.4,
     friction = 0.94,
-    keys = [];
+    radius = 10
 
-// TODO: podzielic na const i let
+let x = width/2,
+    y = height/2,
+    velY = 0,
+    velX = 0,
+    keys = [];
