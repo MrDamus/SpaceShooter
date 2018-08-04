47c5b0bb168bbfd40e83dde5d06fc59f74b75742
Damian <themrdamus@gmail.com>
2018-08-04 18:20:07 +0100

    added UI

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,20 @@
-const playerHp = () => {
-  const ctx = canvas.getContext("2d");
-  ctx.font = '40px kong';
-  ctx.fillText  ('Player HP: ', 0, 270);
+const renderPlayerHP = () => {
+  ctx.font = '15px kong';
+  ctx.textAlign = 'right';
+  ctx.textBaseline = 'middle';
+  ctx.fillText  (`HP: ${player.hp}`, 50, 20);
   const showHp = 'Player'
 }
+
+function renderText() {
+    ctx.font = `18px "${FONT_NAME}"`;
+    ctx.textAlign = 'right';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('Waka Waka', 150, 50);
+}
+
+renderUI = () => {
+  renderPlayerHP();
+}
+
+document.fonts.load('10pt "kong"')
