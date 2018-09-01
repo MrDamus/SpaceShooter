e2d5c178e09a6fe5988374a20b26cde83e4b4a33
Damian <themrdamus@gmail.com>
2018-09-01 21:47:13 +0100

    decreased number of stars

--- js/stars.js
+++ js/stars.js
@@ -1,6 +1,6 @@
 // Set the number of stars to draw
 let stars=[];
-let numStars=100;
+let numStars=30;
 let starSpeed=-5;
 
 // Reset a star
@@ -39,7 +39,7 @@ function updateStars() {
 		ctx.save();
 		ctx.arc(stars[i].x*canvas.width,stars[i].y*canvas.height,stars[i].distance*2,0,2*Math.PI,false);
 		ctx.lineWidth=stars[i].distance*4;
-		ctx.strokeStyle='rgba(255,255,255,0.2)';
+		// ctx.strokeStyle='rgba(255,255,255,0.2)';
 		ctx.stroke();
 		ctx.fillStyle=stars[i].color;
 		ctx.fill();
